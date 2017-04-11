26f919f62587c3ef539202cea903380068db8b0b
amittiwari123 <amitmani213@gmail.com>
2017-04-11 19:25:04 +0530

    User sigin signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords', confirmations: 'users/confirmations', unlocks: 'users/unlocks' }
+
+
   devise_for :admins, controllers: { sessions: 'admins/sessions', registrations: 'admins/registrations', passwords: 'admins/passwords', confirmations: 'admins/confirmations', unlocks: 'admins/unlocks' }
   get 'home/index'
   root 'home#index'
